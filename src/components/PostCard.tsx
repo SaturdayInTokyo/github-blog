aecc56bc418c53e64c1f492887fa2bab230afdaa
Dener Araujo (Natsume) <natsume2b.me@gmail.com>
2023-01-24 18:40:20 -0300

    add PostCard component

---
+++ src/components/PostCard.tsx
@@ -0,0 +1,11 @@
+export function PostCard() {
+  return (
+    <div className="p-8 rounded-xl bg-base-post max-h-64 hover:cursor-pointer hover:outline hover:outline-base-label ">
+      <header className="flex items-center justify-between mb-5">
+        <h1 className="font-bold text-xl">JavaScript data types and data structures</h1>
+        <span className="text-sm text-base-span">Há 1 dia</span>
+      </header>
+      <p className="line-clamp-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero voluptate maxime reprehenderit! Aut harum vel nam deserunt quas? Molestiae exercitationem necessitatibus reiciendis quo suscipit ad cum odio culpa autem aspernatur! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit, blanditiis necessitatibus! Pariatur ratione accusamus a debitis repellat nihil! Unde vel voluptatum esse consectetur ullam? Adipisci quidem recusandae fugiat error eaque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta rerum sed amet ipsa quam, eius beatae! Repudiandae blanditiis eaque odio voluptas quasi porro vel magni labore consequuntur dolorem, minus culpa! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate minus ducimus blanditiis ipsa quis exercitationem, id nobis. Asperiores omnis nesciunt aut beatae dolor. Dolorum corporis laudantium quidem optio nobis odio. </p>
+    </div>
+  )
+}
